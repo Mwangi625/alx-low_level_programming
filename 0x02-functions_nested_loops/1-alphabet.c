fa83f35e082abfe01f4816edd7603a7d2ab5d165
Mwangi625 <mwangiwahome625@gmail.com>
2023-03-22 00:48:02 +0300

    prints the alphabet, in lowercase

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,13 +2,14 @@
 /**
  * main - program entry point
  *
- * function that prints the alphabet, in lowercase, followed by a new line.
+ * function that prints the alphabet, in lowercase, followed by a new line
  *
  * Return: Always 0 for success
  */
 void print_alphabet(void)
 {
 	char i;
+
 	for (i = 'a'; i <= 'z'; i++)
 		_putchar(i);
 	_putchar('\n');
