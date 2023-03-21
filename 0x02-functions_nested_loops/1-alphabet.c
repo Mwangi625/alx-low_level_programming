4ada19d7a0d00f77a0237725b0cb160304b993b4
Mwangi625 <mwangiwahome625@gmail.com>
2023-03-22 00:41:34 +0300

    print_alphabet lowercase

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,12 +6,11 @@
  *
  * Return: Always 0 for success
  */
-int main(void)
+void print_alphabet(void)
 {
 	char i;
 	for (i = 'a'; i <= 'z'; i++)
 		_putchar(i);
 	_putchar('\n');
-	return (0);
 }
-		
+
