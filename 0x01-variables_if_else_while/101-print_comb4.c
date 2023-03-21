ec5efe8852cd6dcf32f52c091e4377294a69cc9e
Mwangi625 <mwangiwahome625@gmail.com>
2023-03-21 08:48:16 -0700

    101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+/**
+ * main - prog entry point
+ *
+ * prog prints all possible cmbntns of 3 dgts
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i = 0;
+
+	int j = 1;
+
+	int k = 2;
+
+	while (i <= 7)
+	{
+		while (j <= 8)
+		{
+			while (k <= 9)
+			{
+				if (i != j && i != k && j != k)
+				{
+					putchar(i + '0');
+					putchar(j + '0');
+					putchar(k + '0');
+					if (i == 7)
+					{
+						putchar('\n');
+						return (0);
+					}
+					putchar(',');
+					putchar(' ');
+				}
+				k++;
+			}
+			j++;
+			k = j + 1;
+		}
+		i++;
+		j = i + 1;
+		k = j + 1;
+	}
+	return (0);
+}
