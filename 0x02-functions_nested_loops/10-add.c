191a74c32f5aa40691bc39ca6e1296d1022b3481
Mwangi625 <mwangiwahome625@gmail.com>
2023-03-22 21:37:31 +0300

    task to 10 and 11

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+*add - is a function to add two int values
+*
+*@x: first int to be added
+*@y: 2nd int to be added
+*
+*Return: (z) for success
+*/
+
+int add(int x, int y)
+{
+
+	int z;
+
+	z = x + y;
+	return (z);
+
+}
