d2ab68c259582f2f138515af10fe53af9f4b7739
Mwangi625 <mwangiwahome625@gmail.com>
2023-03-22 09:44:34 +0300

    betty correction of the command

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,13 +2,15 @@
 /**
  * main - program entry point
  *
- * function that prints the alphabet, in lowercase, followed by a new line
+ *print_alphabet function prints the alphabet, in lowercase, 
+ *followed by a new line
  *
  * Return: Always 0 for success
  */
 void print_alphabet(void)
 {
 	char i;
+
 
 	for (i = 'a'; i <= 'z'; i++)
 		_putchar(i);
