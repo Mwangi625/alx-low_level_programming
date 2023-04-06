5b2c10c2e7ea03c1e921166fa7da251961935664
Mwangi625 <mwangiwahome625@gmail.com>
2023-04-06 16:00:03 +0300

    a function that returns the natural square root of a number

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,40 @@
+#include "main.h"
+/**
+*_sqrt_recursion - a function that returns the
+*natural square root of a number.
+*
+*@n: parameter to test square root
+*
+*Return: int square root
+*/
+
+int _sqrt_recursion(int n)
+{
+	if (n == 1)
+	{
+		return (1);
+	}
+	return (perfectSquare(n, 1));
+}
+
+/**
+*perfectSquare - squares a number
+*
+*@n: parameter to be squared
+*@c: counter parameter
+*
+*Return: int value
+*/
+
+int perfectSquare(int n, int c)
+{
+	if (c < n / 2)
+	{
+		if (c * c == n)
+		{
+			return (c);
+		}
+		return (perfectSquare(n, c + 1));
+	}
+	return (-1);
+}
